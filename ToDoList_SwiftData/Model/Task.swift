92a6a94099968e4e118b42652e4488b809412b5a
emilyj4482 <emilyj4482@gmail.com>
2023-11-07 17:10:00 +0900

    group.tasks array 정렬이 무작위인 것에 대응하기 위해 task model에 Date type 프로퍼티 추가

--- ToDoList_SwiftData/Model/Task.swift
+++ ToDoList_SwiftData/Model/Task.swift
@@ -15,10 +15,14 @@ final class Task {
     var isImportant: Bool
     @Relationship(deleteRule: .nullify) var group: Group?
     
-    init(title: String, isDone: Bool = false, isImportant: Bool = false, group: Group) {
+    // group.tasks array에서 task가 추가된 순이 아닌 무작위로 정렬되는 것을 개선하기 위한 프로퍼티
+    let timestamp: Date
+    
+    init(title: String, isDone: Bool = false, isImportant: Bool = false, group: Group, timestamp: Date = .now) {
         self.title = title
         self.isDone = isDone
         self.isImportant = isImportant
         self.group = group
+        self.timestamp = timestamp
     }
 }
